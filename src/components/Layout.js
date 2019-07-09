d38d04bb9ab7ef4f347363f010367351d7cfb37b
haimengzhang <hzhang1@macalester.edu>
2019-07-08 19:29:20 -0500

    add reconnect function

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client'
-import { USER_CONNECTED, LOGOUT } from '../Events'
+import { USER_CONNECTED, LOGOUT, VERIFY_USER } from '../Events'
 import LoginForm from './LoginForm'
 import ChatContainer from './chats/ChatContainer'
 
-const socketUrl = "http://127.0.0.1:3231"
+const socketUrl = "/"
 export default class Layout extends Component {
 	
 	constructor(props) {
@@ -27,10 +27,24 @@ export default class Layout extends Component {
 		const socket = io(socketUrl)
 
 		socket.on('connect', ()=>{
+			if(tjis.state.user){
+				this.reconnect(socket)
+			} else{
 			console.log("Connected");
+			}
+
 		})
 		
 		this.setState({socket})
+	}
+	reconnect = (socket)=>{
+		socket.emit(VERIFY_USER, this.state.user.name, ({isUser, user})=>{
+			if (isUser){
+				this.setState({user:null})
+			} else {
+				this.setUser(user)
+			}
+		})
 	}
 
 	/*
